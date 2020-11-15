689e0ddc3ef7d31361808a1f469911ef863fc598
ChrisKabbouchy <57867758+ChrisKabbouchy@users.noreply.github.com>
2020-11-15 17:14:09 +0200

    Initial Commit

---
+++ Formula1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Formula1
+//
+//  Created by Christian Kabouchy on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
