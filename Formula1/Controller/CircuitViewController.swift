e5e67e3aa7e6650b393fafbbd0f824ac3fb5417e
ChrisKabbouchy <57867758+ChrisKabbouchy@users.noreply.github.com>
2021-02-11 21:19:54 +0200

    added location pin fixed image and text  in circuit view

--- Formula1/Controller/CircuitViewController.swift
+++ Formula1/Controller/CircuitViewController.swift
@@ -14,7 +14,7 @@ class CircuitViewController: UIViewController {
     
     var racesModel : racesInfoList?
     var index : IndexPath?
-
+    
     @IBOutlet var map: MKMapView!
     @IBOutlet var raceResultButton: UIButton!
     @IBOutlet var raceName: UILabel!
@@ -30,41 +30,38 @@ class CircuitViewController: UIViewController {
     @IBAction func raceResultPressed(_ sender: Any) {
         performSegue(withIdentifier: "goToRaceResult", sender: self)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let Lat = Double(racesModel!.lat) ?? 0
-        let Long = Double(racesModel!.long) ?? 0
-        let center = CLLocationCoordinate2D(latitude: Lat, longitude: Long)
-
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        map.setRegion(region, animated: true)
+        setMap()
         raceName.text = racesModel?.raceName
-        round.text = racesModel?.round
+        round.text = "Round: \(racesModel?.round ?? "")"
         date.text = racesModel?.date
         time.text = racesModel?.time
         flag.image = UIImage(named: racesModel?.country ?? "Country TBC") ?? UIImage(named: "Country TBC")
         countryName.text = racesModel?.country
-        locality.text = racesModel?.locality
+        locality.text = "City: \(racesModel?.locality ?? "")"
         circuitName.text = racesModel?.circuitName
         circuitImage.image = UIImage(named: racesModel?.circuitName ?? "Yas Marina Circuit") ?? UIImage(named: "TBC")
-
+        
         // Do any additional setup after loading the view.
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let resultVC = segue.destination as! RaceResultViewController
         resultVC.round = racesModel?.round
-
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    fileprivate func setMap() {
+        
+        let Lat = Double(racesModel!.lat) ?? 0
+        let Long = Double(racesModel!.long) ?? 0
+        let center = CLLocationCoordinate2D(latitude: Lat, longitude: Long)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        let location = MKPointAnnotation()
+        location.title = racesModel?.circuitName
+        location.coordinate = CLLocationCoordinate2D(latitude: Lat, longitude: Long)
+        map.addAnnotations([location])
+        map.setRegion(region, animated: true)
     }
-    */
-
 }
